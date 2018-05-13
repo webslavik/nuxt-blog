629abbd8c2687e40bd295977e28e7b5bb232e71c
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2018-05-13 08:14:32 +0300

    added markup and styles for Auth component

---
+++ pages/admin/auth/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="admin-auth-page">
+    <div class="auth-container">
+      <form>
+        <app-input type="email">E-Mail Address</app-input>
+        <app-input type="password">Password</app-input>
+        <app-button type="submit">{{ isLogin ? 'Login' : 'Sign Up' }}</app-button>
+        <app-button
+          type="button"
+          btn-style="inverted"
+          style="margin-left: 10px"
+          @click="isLogin = !isLogin">Switch to {{ isLogin ? 'Signup' : 'Login' }}</app-button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppInput from '@/components/UI/AppInput'
+import AppButton from '@/components/UI/AppButton'
+
+export default {
+  name: 'AdminAuthPage',
+  layout: 'admin',
+  components: {
+    AppInput,
+    AppButton
+  },
+  data() {
+    return {
+      isLogin: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.admin-auth-page {
+  padding: 20px;
+}
+
+.auth-container {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 2px #ccc;
+  width: 300px;
+  margin: auto;
+  padding: 10px;
+  box-sizing: border-box;
+}
+</style>
+
